374ee0ed1cf3cf10907bee58a25d36a4f9819d8c
kayljiyan <kylegianlatina@gmail.com>
2025-04-22 20:25:58 +0800

    feat(login): integrated login functions

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -5,22 +5,22 @@ import { createSession, deleteSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 
 const testUser = {
-    id: "1",
-    email: "admin@email.com",
-    password: "12345678",
-    role: "admin" // Add role field
-  };
-  
-  // Example of another user with member role
-  const anotherUser = {
-    id: "2",
-    email: "member@email.com",
-    password: "12345678",
-    role: "member"
-  };
+  id: "1",
+  email: "admin@email.com",
+  password: "12345678",
+  role: "admin", // Add role field
+};
+
+// Example of another user with member role
+const anotherUser = {
+  id: "2",
+  email: "member@email.com",
+  password: "12345678",
+  role: "member",
+};
 
 const loginSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }).trim(),
+  username: z.string().trim(),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters" })
@@ -28,11 +28,11 @@ const loginSchema = z.object({
 });
 
 function getUserByEmail(email: string) {
-    if (email === testUser.email) return testUser;
-    if (email === anotherUser.email) return anotherUser;
-    return null;
-  }
-  
+  if (email === testUser.email) return testUser;
+  if (email === anotherUser.email) return anotherUser;
+  return null;
+}
+
 export async function login(prevState: any, formData: FormData) {
   const result = loginSchema.safeParse(Object.fromEntries(formData));
 
@@ -42,28 +42,48 @@ export async function login(prevState: any, formData: FormData) {
     };
   }
 
-  const { email, password } = result.data;
+  const { username, password } = result.data;
 
-  const user = getUserByEmail(email);
+  // const user = getUserByEmail(email);
+  const url = "https://tfoe-backend.onrender.com/user/login";
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      username: username,
+      password: password,
+    }),
+  });
 
-  if (!user || user.password !== password) {
-    return {
-      errors: {
-        email: ["Invalid email or password"],
-      },
-    };
-  }
-
-  await createSession(user.id, user.role);
-
-  if (user.role === "admin") {
-    redirect("/portal");
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data);
   } else {
-    redirect("/portal-member");
+    console.error(response.status);
   }
+
+  // if (!user || user.password !== password) {
+  //   return {
+  //     errors: {
+  //       email: ["Invalid email or password"],
+  //     },
+  //   };
+  // }
+
+  // await createSession(user.id, user.role);
+  //
+  // if (user.role === "admin") {
+  //   redirect("/portal");
+  // } else {
+  //   redirect("/portal-member");
+  // }
 }
 
 export async function logout() {
   await deleteSession();
   redirect("/login");
 }
+
